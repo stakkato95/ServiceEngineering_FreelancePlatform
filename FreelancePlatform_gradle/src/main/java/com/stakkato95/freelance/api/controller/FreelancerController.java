4cf4e84dcb1de0209d044db86ae75c04c269f07d
Artsiom Kaliaha <stakkato95@gmail.com>
2022-04-18 09:15:50 +0200

    REST. Add GET request for freelancer

--- FreelancePlatform_gradle/src/main/java/com/stakkato95/freelance/api/controller/FreelancerController.java
+++ FreelancePlatform_gradle/src/main/java/com/stakkato95/freelance/api/controller/FreelancerController.java
@@ -2,11 +2,11 @@ package com.stakkato95.freelance.api.controller;
 
 import com.stakkato95.freelance.api.dto.FreelancerDto;
 import com.stakkato95.freelance.api.mapper.FreelancerMapper;
-import com.stakkato95.freelance.domain.Freelancer;
 import com.stakkato95.freelance.repository.FreelancerRepository;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
@@ -31,11 +31,20 @@ public class FreelancerController {
     FreelancerRepository repo;
 
     @Post
-    HttpResponse<Freelancer> create(@Body @Valid FreelancerDto dto) {
+    HttpResponse<FreelancerDto> create(@Body @Valid FreelancerDto dto) {
         var entity = MAPPER.toEntity(dto);
-        var freelancer = repo.save(entity);
+        var savedEntity = repo.save(entity);
+        var savedDto = MAPPER.toDto(savedEntity);
         return HttpResponse
-                .created(freelancer)
-                .headers(headers -> headers.location(createdLocation(ENDPOINT, freelancer.getId())));
+                .created(savedDto)
+                .headers(headers -> headers.location(createdLocation(ENDPOINT, savedDto.getId())));
+    }
+
+    @Get("/{id}")
+    HttpResponse<FreelancerDto> get(Long id) {
+        return repo.findById(id)
+                .map(MAPPER::toDto)
+                .map(HttpResponse::ok)
+                .orElse(HttpResponse.notFound());
     }
 }
