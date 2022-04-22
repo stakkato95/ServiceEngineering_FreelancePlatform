6e01a02a72e31e7bf688e6ffd0035288dd29e857
Artsiom Kaliaha <stakkato95@gmail.com>
2022-04-22 11:44:05 +0200

    OpenAPI. Add outline of api

--- FreelancePlatform_gradle/src/main/java/com/stakkato95/freelance/Application.java
+++ FreelancePlatform_gradle/src/main/java/com/stakkato95/freelance/Application.java
@@ -1,12 +1,29 @@
 package com.stakkato95.freelance;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Freelance platform",
+//                version = "0.1",
+                description = "API for accessing API platform",
+                license = @License(name = "Apache 2.0"),
+                contact = @Contact(name = "Kaliaha Artsiom", email = "stakkato95@gmail.com")
+        )
+)
+//@OpenAPIInclude(
+//        classes = { io.micronaut.security.endpoints.LoginController.class, io.micronaut.security.endpoints.LogoutController.class },
+//        tags = @Tag(name = "Security")
+//)
 public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
-//    5 platform-port instead of "8080"
-//    6 db url in app injecten. DbConfig
 }
