8d3da2623067710a1e0edf7e545c33ec407332c7
Artsiom Kaliaha <stakkato95@gmail.com>
2022-04-22 09:44:45 +0200

    k8s. Finish liveness and readiness probe

--- FreelancePlatform_gradle/src/main/java/com/stakkato95/freelance/Application.java
+++ FreelancePlatform_gradle/src/main/java/com/stakkato95/freelance/Application.java
@@ -7,4 +7,6 @@ public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
+//    5 platform-port instead of "8080"
+//    6 db url in app injecten. DbConfig
 }
